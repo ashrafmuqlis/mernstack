c5b050467a338a66d60e95d469d874f3f133ccd2
ashrafmuqlis <ashrafmuqlis@gmail.com>
2023-01-17 15:32:12 +0800

    Update vagrant_mern_setup.sh

--- vagrant_mern_setup.sh
+++ vagrant_mern_setup.sh
@@ -42,22 +42,25 @@ sudo npm init -y
 sudo npm install mongodb express cors dotenv
 
 echo "Creating server.js file"
-sudo echo -e "const express = require("express");\n
-const app = express();\n
-const cors = require("cors");\n
-require("dotenv").config({ path: "./config.env" });\n
-const port = process.env.PORT || 5000;\n
-app.use(cors());\n
-app.use(express.json());\n
-app.use(require("./routes/record"));\n
-// get driver connection\n
-const dbo = require("./db/conn");\n
+sudo echo "
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/record"));
+// get driver connection
+const dbo = require("./db/conn");
  \n
-app.listen(port, () => {\n
-  // perform a database connection when server starts\n
-  dbo.connectToServer(function (err) {\n
-    if (err) console.error(err);\n
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
  \n
-  });\n
-  console.log(`Server is running on port: ${port}`);\n
+  });
+  console.log(`Server is running on port: ${port}`);
 });" > server.js
+
+

0138784a4b50beb1e6cc72050d161f0ff8efaf92
ashrafmuqlis <ashrafmuqlis@gmail.com>
2023-01-17 17:31:59 +0800

    Update import.sh

--- import.sh
+++ import.sh
@@ -62,7 +62,7 @@ const express = require(\"express\");
  
 // recordRoutes is an instance of the express router.
 // We use it to define our routes.
-// The router will be added as a middleware and will take control of requests starting with path \/record.
+// The router will be added as a middleware and will take control of requests starting with path /record.
 const recordRoutes = express.Router();
  
 // This will help us connect to the database
@@ -134,7 +134,7 @@ recordRoutes.route(\"/update/:id\").post(function (req, response) {
 recordRoutes.route(\"/:id\").delete((req, response) => {
  let db_connect = dbo.getDb();
  let myquery = { _id: ObjectId(req.params.id) };
- db_connect.collection("records").deleteOne(myquery, function (err, obj) {
+ db_connect.collection(\"records\").deleteOne(myquery, function (err, obj) {
    if (err) throw err;
    console.log(\"1 document deleted\");
    response.json(obj);

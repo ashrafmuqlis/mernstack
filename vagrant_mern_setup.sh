6a5b2856f8b6fd53f4883a61baccb0d9244cec3d
ashrafmuqlis <ashrafmuqlis@gmail.com>
2023-01-17 15:36:57 +0800

    Update vagrant_mern_setup.sh

--- vagrant_mern_setup.sh
+++ vagrant_mern_setup.sh
@@ -40,27 +40,3 @@ mkdir server
 cd server
 sudo npm init -y
 sudo npm install mongodb express cors dotenv
-
-echo "Creating server.js file"
-sudo echo "
-const express = require("express");
-const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
-app.use(cors());
-app.use(express.json());
-app.use(require("./routes/record"));
-// get driver connection
-const dbo = require("./db/conn");
- \n
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- \n
-  });
-  console.log(`Server is running on port: ${port}`);
-});" > server.js
-
-

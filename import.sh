880472c7d654e530bb8b30bdf1717b050b2434fa
ashrafmuqlis <ashrafmuqlis@gmail.com>
2023-01-17 15:40:29 +0800

    Update import.sh

--- import.sh
+++ import.sh
@@ -1,15 +1,15 @@
 echo "Creating server.js file"
 sudo echo "
-const express = require("express");
+const express = require(\"express\");
 const app = express();
-const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+const cors = require(\"cors\");
+require("dotenv").config({ path: \"./config.env\" });
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/record"));
+app.use(require(\"./routes/record\"));
 // get driver connection
-const dbo = require("./db/conn");
+const dbo = require(\"./db/conn\");
  \n
 app.listen(port, () => {
   // perform a database connection when server starts
@@ -17,5 +17,5 @@ app.listen(port, () => {
     if (err) console.error(err);
  \n
   });
-  console.log(`Server is running on port: ${port}`);
+  console.log(\`Server is running on port: ${port}\`);
 });" > server.js
